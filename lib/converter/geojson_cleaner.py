a4619fa67bfa174810da99756225d20cddb46f97
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-02-08 16:25:27 +0100

    fix: geometry cleaning #8

--- lib/converter/geojson_cleaner.py
+++ lib/converter/geojson_cleaner.py
@@ -1,5 +1,7 @@
 import json
 import os
+from collections import Sequence
+from itertools import chain, count
 from pathlib import Path
 
 from tracking_decorator import TrackingDecorator
@@ -26,7 +28,8 @@ def unify_properties(geojson):
                 properties.pop(id_property, None)
 
         # Iterate over potential name properties
-        for name_property in ["Gemeinde_name", "PROGNOSERA", "PGR_NAME", "BEZIRKSREG", "BZR_NAME", "PLANUNGSRA", "PLR_NAME"]:
+        for name_property in ["Gemeinde_name", "PROGNOSERA", "PGR_NAME", "BEZIRKSREG", "BZR_NAME", "PLANUNGSRA",
+                              "PLR_NAME"]:
             if name_property in properties:
                 name = properties[name_property]
                 properties.pop(name_property, None)
@@ -50,22 +53,44 @@ def unify_properties(geojson):
     return changed
 
 
-def clean_geometry(geojson):
+def clean_geometry(logger, geojson, quiet):
     changed = False
 
     for feature in geojson["features"]:
+        id = feature["properties"]["id"]
+        name = feature["properties"]["name"]
         polygons = feature["geometry"]["coordinates"]
 
+        # Check if there is more than one polygon
         if len(polygons) > 1:
             polygon_max = [[]]
-            for polygon in polygons:
-                if len(polygon[0]) > len(polygon_max[0]):
-                    polygon_max = [polygon]
 
-            feature["geometry"]["coordinates"] = polygon_max
+            for polygon in polygons:
+                polygon_elements_count = len(polygon[0])
+                polygon_elements_count_max = len(polygon_max[0])
+
+                if polygon_elements_count > polygon_elements_count_max:
+                    polygon_max = polygon
+
+            feature["geometry"]["coordinates"] = [polygon_max]
             changed = True
 
+            if not quiet:
+                logger.log_line(f"⚠ Clean geometry of {id} {name}")
+
+        # Sanity-check geometry
+        if get_depth(feature["geometry"]["coordinates"]) != 4 and not quiet:
+            logger.log_line(f"⚠ Invalid geometry of {id} {name}")
+
     return changed
+
+
+# Thanks https://stackoverflow.com/a/6040217/2992219
+def get_depth(seq):
+    for level in count():
+        if not seq:
+            return level
+        seq = list(chain.from_iterable(s for s in seq if isinstance(s, Sequence)))
 
 
 #
@@ -92,7 +117,7 @@ class GeojsonCleaner:
 
                 changed = False
                 changed |= unify_properties(geojson)
-                changed |= clean_geometry(geojson)
+                changed |= clean_geometry(logger, geojson, quiet)
 
                 if changed:
                     with open(results_file_path, "w") as geojson_file:
